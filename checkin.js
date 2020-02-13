f764c99aa709b46b65ad4bc17b1aef04f94c636b
BeemoLin <dteout@gmail.com>
2020-02-13 14:06:13 +0800

    MODIFY: day time check

--- checkin.js
+++ checkin.js
@@ -1,24 +1,33 @@
 require('dotenv').config();
 const qs = require('querystring');
 const request = require('request');
+const moment = require('moment');
 
 let username = process.env["USERNAME"];
 let password = process.env["PASSWORD"];
 let user_id = process.env["USER_ID"];
 let delayMinMinute = process.env["DELAY_MIN_MINUTE"];
 let delayMaxMinute = process.env["DELAY_MAX_MINUTE"];
+let enable_day = process.env["ENABLE_DAY"].split(",") || '';
+let disable_day = process.env["DISABLE_DAY"].split(",") || '';
 
 console.log("username: ", username);
+console.log("enable_day", enable_day);
+console.log("disable_day", disable_day);
 
 function checkin(token) {
 	const checkin_url = "https://femascloud.com/vizuro/users/clock_listing";
+
+	// check AM or PM
+	const in_out = new Date().getHours() >= 12 ? 'E' : 'S';
+	console.log(in_out === 'E' ? '下班' : '上班');
 
 	const requestBody = {
 	  'data[ClockRecord][user_id]': user_id,
 	  'data[AttRecord][user_id]': user_id,
 	  'data[ClockRecord][shift_id]': '2',
 	  'data[ClockRecord][period]': '1',
-	  'data[ClockRecord][clock_type]': 'S',
+	  'data[ClockRecord][clock_type]': in_out,
 	  'data[ClockRecord][latitude]': '',
 	  'data[ClockRecord][longitude]': ''
 	};
@@ -88,7 +97,36 @@ function getRandomMinute(min,max){
   return Math.floor(Math.random()*(maxMinute-minMinute+1))+minMinute;
 }
 
-let delayTime = getRandomMinute(delayMinMinute, delayMaxMinute);
-console.log("delayTime:", delayTime);
+function isWeekDay(weekday) {
+  weekday = weekday || moment().weekday();
+  console.log("weekday:", weekday);
 
-delay(login, delayTime);
+  return (weekday >= 1 && weekday <= 5);
+}
+
+function isWorkDay() {
+  let today = moment().format('YYYYMMDD');
+  console.log("today", today);
+
+  if(disable_day.includes(today)) {
+    console.log("不上班");
+    return false;
+  }
+
+  if(enable_day.includes(today)) {
+    console.log("要上班");
+    return true;
+  }
+
+  return isWeekDay();
+}
+
+if(isWorkDay()) {
+  console.log("今天要上班");
+  let delayTime = getRandomMinute(delayMinMinute, delayMaxMinute);
+  console.log("delayTime:", delayTime);
+  delay(login, delayTime);
+} else {
+  console.log("今天不上班");
+}
+
